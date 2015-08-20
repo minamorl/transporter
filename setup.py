ea7e30df51039fa5b18b37fb5049dd8f4dd0e9a9
minamorl <minamorl@users.noreply.github.com>
2015-08-21 06:01:34 +0900

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='transporter',
-    version='0.3.0',
+    version='0.4.0',
     packages=['transporter'],
     entry_points={
             'console_scripts': ['transporter = transporter.__main__:main']
