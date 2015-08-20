13e09bbf645b53f75627de92c2dce1787ca0d6fe
minamorl <minamorl@users.noreply.github.com>
2015-08-21 07:59:21 +0900

    chore: remove old dependency

--- setup.py
+++ setup.py
@@ -7,4 +7,10 @@ setup(
     entry_points={
             'console_scripts': ['transporter = transporter.__main__:main']
     },
+    author='minamorl',
+    author_email='minamorl@users.noreply.github.com',
+    install_requires=[
+        'boto3',
+        'pillow',
+    ],
 )
--- transporter/__main__.py
+++ transporter/__main__.py
@@ -1,5 +1,4 @@
 import os
-import paramiko
 import hashlib
 import sys
 import clint
