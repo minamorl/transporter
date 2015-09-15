23fd374d32ce5c5e27e9bd1a6746f07449f9a915
minamorl <minamorl@users.noreply.github.com>
2015-09-16 03:00:53 +0900

    feat: add size option to compress func

--- transporter/compress.py
+++ transporter/compress.py
@@ -14,9 +14,8 @@ def _detect_valid_format(path):
     return {".jpg": "jpeg", ".png": "png"}.get(ext)
 
 
-def compress(path):
+def compress(path, size=(1024, 1024)):
     im = Image.open(path)
-    size = (1024, 1024)
     im.thumbnail(size, Image.ANTIALIAS)
     with NamedTemporaryFile(delete=False) as temp:
         im.save(temp, _detect_valid_format(path))
